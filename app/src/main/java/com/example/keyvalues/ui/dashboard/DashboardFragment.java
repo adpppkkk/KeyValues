366531156ede1a30ccf2dc269dc110387731b58b
sihong Guo <yellowjerry6@gmail.com>
2019-09-30 09:53:04 -0500

    Basically finish all requirements

--- app/src/main/java/com/example/keyvalues/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/keyvalues/ui/dashboard/DashboardFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -24,6 +25,8 @@ public class DashboardFragment extends Fragment {
     public View Textview;
     public TextView Time,ClickCount;
     private int counter;
+    private CountDownTimer countdowntimer;
+    private long timems = 30000; //30 sec
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -31,47 +34,83 @@ public class DashboardFragment extends Fragment {
                 ViewModelProviders.of(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
         final TextView textView = root.findViewById(R.id.text_dashboard);
-        final TextView Time = root.findViewById(R.id.textView);
+        final TextView Time = root.findViewById(R.id.timer);
         final TextView ClickCount = root.findViewById(R.id.counter);
+        SharedPreferences Preference = getActivity().getSharedPreferences("SCORE", Context.MODE_PRIVATE);
+        final int Bestsocre = Preference.getInt("Bestscore",0);
         dashboardViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+                textView.setText("The best score is "+ Bestsocre + " clicks!");
             }
 
 
         });
 
 
-        Button b_start = root.findViewById(R.id.Start);
-        Button b_trigger = root.findViewById(R.id.Trigger);
+        final Button b_start = root.findViewById(R.id.Start);
+        final ImageButton b_trigger = root.findViewById(R.id.trigger_button);
 
-        //b_trigger.setEnabled(false);
+        b_trigger.setEnabled(false);
+        ClickCount.setText("0 clicks!");
+        Time.setText("30 sec");
 
-/*        b_start.setOnClickListener(new View.OnClickListener() {
+
+        b_start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //b_start.setVisibility(View.GONE);
-                //b_trigger.setEnabled(true);
+                b_start.setVisibility(View.GONE);
+                b_trigger.setEnabled(true);
                 counter = 0;
 
-
-                //trigger start
-                b_trigger.setOnClickListener(new View.OnClickListener() {
+                countdowntimer = new CountDownTimer(timems,1000) {
                     @Override
-                    public void onClick(View view) {
-                        counter++;
-                        ClickCount.setText(counter + "clicks!");
+                    public void onTick(long millisUntilFinished) {
+                        timems = millisUntilFinished;
+                        int seconds = (int) timems/1000;
+                        Time.setText(seconds+" sec");
+
+                        //trigger start
+                        b_trigger.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                counter++;
+                                ClickCount.setText(counter + " clicks!");
+                            }
+                        });
                     }
-                });
+
+                    @Override
+                    public void onFinish() {
+                        b_trigger.setEnabled(false);
+                        /*SharedPreferences Preference = getActivity().getSharedPreferences("SCORE", Context.MODE_PRIVATE);
+                        int Bestsocre = Preference.getInt("Bestscore",0);*/
+
+                        if(counter > Bestsocre){
+                            SharedPreferences.Editor editor = getActivity().getSharedPreferences("SCORE",Context.MODE_PRIVATE).edit();
+                            editor.putInt("Bestscore",counter);
+                            editor.commit();
+                            textView.setText("The best score is "+ counter + " clicks!");
+                        }
+                        else{
+                            textView.setText("The best score is "+ Bestsocre + " clicks!");
+                        }
+                    }
+                }.start();
+
+
 
             }
         });
 
-*/
+
+
 
 
 
         return root;
     }
+
+
+
 }
